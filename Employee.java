506388bdf0adcd9d17098aa85e432a641bdf9328
Pradeep956643 <pradeeprajendiren@gmail.com>
2019-11-11 15:32:23 +0530

    New Empoyee has been Added

---
+++ Employee.java
@@ -0,0 +1,57 @@
+package org.emp;
+
+public class Employee {
+
+	/*int empId;
+	String empName, empDob, empPhone, empEmail, empAddress;*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee emp = new Employee();
+
+		emp.empId();
+		emp.empName();
+		emp.empDob();
+		emp.empPhone();
+		emp.empEmail();
+		emp.empAddress();
+		emp.someName();
+
+	}
+
+	public void empId() {
+		System.out.println("empId");
+	}
+
+	public void empName() {
+		System.out.println("empName");
+	}
+
+	public void empDob() {
+		System.out.println("empDob");
+	}
+
+	public void empPhone() {
+		System.out.println("empPhone");
+	}
+
+	public void empEmail() {
+		System.out.println("empEmail");
+	}
+
+	public void empAddress() {
+		System.out.println("empAddress");
+	}
+	
+	public void someName() {
+		
+		System.out.println("My employee file has been edited");
+		
+		System;
+	}
+	
+	
+	
+
+}
